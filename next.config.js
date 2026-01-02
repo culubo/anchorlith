fd1913787a5a82ccac14697d8d836b41b8103561
culubo <1culubo@gmail.com>
2026-01-01 22:02:01 -0500

    Update ESLint config: make problematic rules warnings and update Next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Don't fail build on ESLint errors during production builds
+    // Warnings will still be shown but won't block deployment
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    // Don't fail build on TypeScript errors
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig
