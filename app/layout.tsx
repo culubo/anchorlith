1bc03cbd2d933ebfa60d3d1e3a3a2ba80b21f14d
culubo <1culubo@gmail.com>
2026-01-03 15:11:46 -0500

    chore: add favicon and link in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, viewport-fit=cover" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
       </head>
       <body className={inter.variable}>
         <ThemeProvider>{children}</ThemeProvider>
